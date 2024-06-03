a26a6820614e0f285abb5f69e4708ad4994279b0
DESKTOP-HAGMG1Q\user <dongww12@gmail.com>
2024-06-03 11:32:02 +0900

    remove Stub class

--- DriverTest/test.cpp
+++ DriverTest/test.cpp
@@ -4,19 +4,10 @@
 
 using namespace testing;
 
-class StubFlashMemoryDevice : public FlashMemoryDevice {
-public:
-	unsigned char read(long address) override {
-		return 0;
-	}
-	void write(long address, unsigned char data) override {
-		return;
-	}
-};
-
-class MockFlashMemoryDevice : public StubFlashMemoryDevice {
+class MockFlashMemoryDevice : public FlashMemoryDevice {
 public:
 	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
 };
 
 class DriverTextFixture : public testing::Test {
@@ -24,43 +15,40 @@ public:
 	long address = 0x01;
 	int data = 7;
 	MockFlashMemoryDevice device;
+
+	DeviceDriver driver{ &device };
 };
 
 TEST_F(DriverTextFixture, Read) {
-	EXPECT_CALL(device, read(address))
-		.Times(AtLeast(5));
+	EXPECT_CALL(device, read)
+		.Times(5);
 
-	DeviceDriver driver(&device);
-	int actual = driver.read(address);
-	int expected = 0;
-	EXPECT_EQ(actual, expected);
+	EXPECT_EQ(driver.read(address), 0);
 }
 
 TEST_F(DriverTextFixture, Read2) {
+	constexpr int returnValure = 7;
 	EXPECT_CALL(device, read(address))
 		.Times(AtLeast(5))
-		.WillRepeatedly(Return(7));
+		.WillRepeatedly(Return(returnValure));
 
-	DeviceDriver driver(&device);
-	int actual = driver.read(address);
-	int expected = 7;
-	EXPECT_EQ(actual, expected);
+	EXPECT_EQ(driver.read(address), returnValure);
 }
 
 TEST_F(DriverTextFixture, ReadFail) {
+	constexpr int returnValure = 7;
 	EXPECT_CALL(device, read(address))
 		.WillOnce(Return(5))
-		.WillRepeatedly(Return(7));
-
-	DeviceDriver driver(&device);
+		.WillRepeatedly(Return(returnValure));
+	
 	EXPECT_THROW(driver.read(address), ReadFailException);
 }
 
 TEST_F(DriverTextFixture, Write) {
 	EXPECT_CALL(device, read(address))
 		.WillRepeatedly(Return(0xFF));
+	EXPECT_CALL(device, write(address, _));
 
-	DeviceDriver driver(&device);
 	driver.write(address, data);
 	EXPECT_TRUE(true);
 }
@@ -69,6 +57,5 @@ TEST_F(DriverTextFixture, WriteFail) {
 	EXPECT_CALL(device, read(address))
 		.WillRepeatedly(Return(0xF0));
 
-	DeviceDriver driver(&device);
 	EXPECT_THROW(driver.write(address, data), WriteFailException);
 }
