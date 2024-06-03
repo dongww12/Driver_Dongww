750dcde6296477cb3d8cc0298f75fbd57d5a07e0
DESKTOP-HAGMG1Q\user <dongww12@gmail.com>
2024-06-03 10:11:29 +0900

    implement DeviceDriver

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,16 +1,33 @@
 ﻿#include "DeviceDriver.h"
+//#include <stdexception>
+
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
+class ReadFailException {
+
+};
+
+class WriteFailException {
+
+};
+
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    constexpr int numRead = 5;
+    int result = (int)(m_hardware->read(address));
+    for (int i = 1; i < numRead; ++i) {
+        if (result != (int)(m_hardware->read(address)))
+            throw ReadFailException();
+    }
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
+    if (read(address) != 0xFF)
+        throw WriteFailException();
     m_hardware->write(address, (unsigned char)data);
 }
--- DriverTest/test.cpp
+++ DriverTest/test.cpp
@@ -2,7 +2,73 @@
 #include "gmock/gmock.h"
 #include "../DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
+using namespace testing;
+
+class StubFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	unsigned char read(long address) override {
+		return 0;
+	}
+	void write(long address, unsigned char data) override {
+		return;
+	}
+};
+
+class MockFlashMemoryDevice : public StubFlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+};
+
+class DriverTextFixture : public testing::Test {
+public:
+	long address = 0x01;
+	int data = 7;
+	MockFlashMemoryDevice device;
+};
+
+TEST_F(DriverTextFixture, Read) {
+	EXPECT_CALL(device, read(address))
+		.Times(AtLeast(5));
+
+	DeviceDriver driver(&device);
+	int actual = driver.read(address);
+	int expected = 0;
+	EXPECT_EQ(actual, expected);
+}
+
+TEST_F(DriverTextFixture, Read2) {
+	EXPECT_CALL(device, read(address))
+		.Times(AtLeast(5))
+		.WillRepeatedly(Return(7));
+
+	DeviceDriver driver(&device);
+	int actual = driver.read(address);
+	int expected = 7;
+	EXPECT_EQ(actual, expected);
+}
+
+TEST_F(DriverTextFixture, ReadFail) {
+	EXPECT_CALL(device, read(address))
+		.WillOnce(Return(5))
+		.WillRepeatedly(Return(7));
+
+	DeviceDriver driver(&device);
+	EXPECT_THROW(driver.read(address), ReadFailException);
+}
+
+TEST_F(DriverTextFixture, Write) {
+	EXPECT_CALL(device, read(address))
+		.WillRepeatedly(Return(0xFF));
+
+	DeviceDriver driver(&device);
+	driver.write(address, data);
 	EXPECT_TRUE(true);
+}
+
+TEST_F(DriverTextFixture, WriteFail) {
+	EXPECT_CALL(device, read(address))
+		.WillRepeatedly(Return(0xF0));
+
+	DeviceDriver driver(&device);
+	EXPECT_THROW(driver.write(address, data), WriteFailException);
 }
